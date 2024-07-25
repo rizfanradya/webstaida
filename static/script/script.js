8d552a5305a62143e71aeae02f873d364b70d2dc
rizfanradya <rizfankusuma@gmail.com>
2024-07-25 08:31:49 +0000

    feat: Add lazy loading for news carousel images
    Update the code in `script.js` and `dashboard.html` to implement lazy loading for the images in the news carousel. This improves the page load performance by loading images only when they are visible to the user.

--- static/script/script.js
+++ static/script/script.js
@@ -1,5 +1,19 @@
-new Splide(".splide", {
+new Splide("#splide_dashboard", {
   type: "loop",
   perPage: 1,
   autoplay: true,
+}).mount();
+
+new Splide("#splide_news", {
+  type: "loop",
+  perPage: 3,
+  autoplay: true,
+  breakpoints: {
+    768: {
+      perPage: 1,
+    },
+    1024: {
+      perPage: 2,
+    },
+  },
 }).mount();
